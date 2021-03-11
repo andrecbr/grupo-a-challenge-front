b6f12eac82eec027ad0638c783e3d23a1cc6395a
Andre Clemer <andreclemer@gmail.com>
2021-03-11 01:35:07 -0300

    fix(config): Ajustada URL da API para HTTPS

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://grupo-a-challenge.herokuapp.com/api",
+  baseURL: "https://grupo-a-challenge.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   },
